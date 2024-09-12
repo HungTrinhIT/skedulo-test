a92b6cb67ec3eab8ea6e2cde3884b75c80a54d7e
HungTrinhIT <minhhung.it.work@gmail.com>
2024-09-13 04:34:40 +0700

    disable IOS auto zoom input field

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -33,7 +33,7 @@ const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
               value={value}
               type='search'
               id='default-search'
-              className='outline-none block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className='outline-none block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-[16px]'
               placeholder='Find github user all around the world ...'
             />
             <Button
